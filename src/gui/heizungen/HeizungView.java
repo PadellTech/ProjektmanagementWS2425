1e10db50b6be8dffdda75fd57d8ce1d3a3621280
PadellTech <146084479+PadellTech@users.noreply.github.com>
2024-11-28 21:25:13 +0100

    Preise ergaenzt

--- src/gui/heizungen/HeizungView.java
+++ src/gui/heizungen/HeizungView.java
@@ -48,6 +48,7 @@ public class HeizungView extends BasisView{
             txtPreisPlatzhalter[i] = new TextField();
             lblPlatzhalterEuro[i] = new Label("Euro");
             chckBxPlatzhalter[i] = new CheckBox();
+            txtPreisPlatzhalter[i].setText(sonderwuensche[i][1]);
 
             // Setze Textfelder auf nicht editierbar
             txtPreisPlatzhalter[i].setEditable(false);
